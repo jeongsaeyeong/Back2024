b8ffd598696cf5fc891b5f12b50f10041a44bba0
jeongsaeyeong <140478779+jeongsaeyeong@users.noreply.github.com>
2023-10-25 16:46:28 +0900

    Add files via upload

---
+++ team/php/include/header.php
@@ -0,0 +1,17 @@
+<header id="header" class="leftHeader shaDow">
+    <h1 class="header_logo">
+        <a href="index.html">
+            <img src="../assets/img/logo.svg" alt="logo">
+        </a>
+    </h1>
+    <nav class="header_nav" role="navigation" aria-label="main">
+        <ul>
+            <li><a href="index.html"><i class="fa-solid fa-house"></i>홈</a></li>
+            <li><a href="alcohol_list.html"><i class="fa-regular fa-heart"></i> 술 리뷰</a></li>
+            <li><a href="board.html"><i class="fa-regular fa-comments"></i> 자유 게시판</a></li>
+            <li><a href="event.html"><i class="fa-solid fa-gift"></i> 이벤트</a></li>
+            <li><a href="mypage.html"><i class="fa-regular fa-user"></i> 마이페이지</a></li>
+            <li><a href="setting.html"><i class="fa-solid fa-gear"></i> 설정</a></li>
+        </ul>
+    </nav>
+</header>
